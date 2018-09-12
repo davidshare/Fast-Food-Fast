ca96cbe0cba3db7336bd94fc8c29762084269737
davidshare <davidessienshare@gmail.com>
2018-09-12 06:37:25 +0100

    chore(validateOrder): add validation for orders
    - create a validation middleware class for orders
    - create static method to validate orders
    - create static method to validate orders staus update
    - create static method to validate orderId for getting orders
    - add middware to routes in the routes index file
    
    Finishes #160437337

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
 import OrdersController from '../controllers/OrdersController';
+import ValidateOrder from '../middleware/orderValidator';
 
 /**
  * @fileOverview index file for routes - it hosts all routes
  * @requires OrdersController
  * @requires ../controllsers/OrdersController
+ * * @requires ../middleware/orderValidator
  * @param {object} app
  * @exports routes What is exported
  */
@@ -16,10 +18,10 @@ const routes = (app) => {
   }));
 
   app.get('/api/v1/orders', OrdersController.getAllOrders);
-  app.get('/api/v1/orders/:orderId', OrdersController.getOrderById);
+  app.get('/api/v1/orders/:orderId', ValidateOrder.validateOrderId, OrdersController.getOrderById);
 
-  app.post('/api/v1/orders', OrdersController.postOrder);
+  app.post('/api/v1/orders', ValidateOrder.validateOrder, OrdersController.postOrder);
 
-  app.put('/api/v1/orders/:orderId', OrdersController.updateOrderStatus);
+  app.put('/api/v1/orders/:orderId', ValidateOrder.validateOrderStatus, OrdersController.updateOrderStatus);
 };
 export default routes;
