7299946069a869cdc76ff06fe7e3f171d1f7d8b4
davidshare <davidessienshare@gmail.com>
2018-09-20 02:26:52 +0100

    add postgres database configuration to travis file

--- server/models/index.js
+++ server/models/index.js
@@ -7,7 +7,6 @@ client.connect();
 
 const dbQueries = `${destroyQuery}${createQuery}`;
 
-client.query(dbQueries, (error) => {
-  console.log(error);
+client.query(dbQueries, () => {
   client.end();
 });
