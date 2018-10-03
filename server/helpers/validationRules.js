b9fef1ec7bfae1483ca133176506307ab75e2a66
davidshare <davidessienshare@gmail.com>
2018-10-03 15:34:06 +0100

    feature(mealImage): add image for new meal
    - refactor the addMeal method of the MealsController to include a picture field
    - add validation for the meal image field
    - write tests for the user input for the meal image field
    - add regular expression for validating meal url to validationRules
    - include the meal image url to the test data for meals
    
    Finishes #160958275

--- server/helpers/validationRules.js
+++ server/helpers/validationRules.js
@@ -3,7 +3,7 @@ const rules = {
   validAddress: /^[a-zA-Z][a-zA-Z0-9\s?.,:]+$/,
   empty: /^(\S+)/,
   addressLength: /^[a-zA-Z][a-zA-Z0-9\s?.,:]{10,150}$/,
-  validEmail: /^[A-Za-z]\w{3,15}@\w{2,15}[.]\w{2,15}$/,
+  validEmail: /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/,
   nameLength: /^[a-zA-Z][a-zA-Z\s]{8,}$/,
   validNumber: /^[0-9]{8,15}/,
   validId: /^[1-9]{1,}/,
@@ -13,6 +13,11 @@ const rules = {
   validDesc: /^[a-zA-Z][a-zA-Z0-9!$&%\\\s`()*.+,'-]+$/,
   mealLength: /^[a-zA-Z][a-zA-Z0-9\s]{5,50}$/,
   descLength: /^[a-zA-Z][a-zA-Z0-9!$&%\\\s`()*.+,'-]{39,255}$/,
+  validUrl: /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/,
 };
 
 export default rules;
+
+// url regex: https://www.regextester.com/93652
+// email regex: https://www.regextester.com/1922
+
