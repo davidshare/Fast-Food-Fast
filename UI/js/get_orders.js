aff36a11acec03582dba26a93678dd0bbc36a64f
davidshare <davidessienshare@gmail.com>
2018-10-16 16:43:41 +0100

    feature(ordersFrontend): get orderf from frontend
    - create a file and add a function for getting orders from the database
    - style the orders being displayed
    - remove dummy data and replace it with order from the database
    - write a function to display action buttons for updating the order status
    - style the buttons
    
    Finishes #160876074

---
+++ UI/js/get_orders.js
@@ -0,0 +1,26 @@
+const getOrders = () => {
+  fetch(ordersURL, {
+    method: 'GET',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-access': getUserToken(),
+    },
+  })
+    .then((response) => {
+      return response.json()
+        .then((ordersResponse) => {
+          if (response.ok) return ordersResponse;
+          throw ordersResponse;
+        })
+        .then((data) => {
+          displayOrders(data.orders, 'orders-container');
+        })
+    })
+    .catch((error) => {
+      if (error.status === 404) {
+        showMessage(error.error, 'error-text');
+      }
+    });
+};
+getOrders();
