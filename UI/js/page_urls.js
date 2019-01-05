a901df2620923b081836bc4207c2f2e0d7ba5ab4
davidshare <davidessienshare@gmail.com>
2019-01-05 07:32:56 +0100

    ch(herokuURL): change the urls on the front end from localhost to heroku
    - comment out the links for localhost in the page_urls files
      and replace them with the heroku links
    
    [Finishes #163001333]

--- UI/js/page_urls.js
+++ UI/js/page_urls.js
@@ -1,9 +1,9 @@
-// const baseUrl = 'https://fast-food-fast-essien.herokuapp.com/api/v1/';
-// const appUrl = 'https://fast-food-fast-essien.herokuapp.com/';
-const appUrl = 'http://localhost:3000/';
-const baseUrl = 'http://localhost:3000/api/v1/';
+const baseUrl = 'https://fast-food-fast-essien.herokuapp.com/api/v1/';
+const appUrl = 'https://fast-food-fast-essien.herokuapp.com/';
+// const appUrl = 'http://localhost:3000/';
+// const baseUrl = 'http://localhost:3000/api/v1/';
 const CLOUDINARY_UPLOAD_PATH = 'https://api.cloudinary.com/v1_1/degd8xqnf/image/upload';
-const CLOUDINARY_UPLOAD_PRESET ='knowshare';
+const CLOUDINARY_UPLOAD_PRESET = 'knowshare';
 
 /* API endpoints */
 const signupURL = `${baseUrl}auth/signup`;
@@ -11,7 +11,7 @@ const loginURL = `${baseUrl}auth/login`;
 const menuUrl = `${baseUrl}menu`;
 const ordersURL = `${baseUrl}orders`;
 
-/* HTML PAGES*/
+/* HTML PAGES */
 const landingPage = 'index.html';
 const adminSignUpPage = 'new_admin.html';
 const loginPage = 'signin.html';
